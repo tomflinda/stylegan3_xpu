7e16b3ca88eff1ff109d0d1cdd617d46e378a2af
tomflinda <tomflinda@gmail.com>
2024-10-17 11:37:07 +0800

    Add guideline doc for the migration of cuda operators and build & run migrated SYCL operator (#7)
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

---
+++ migration_assist_folder/migrate.sh
@@ -0,0 +1,3 @@
+dpct -in-root /home/chenweis/.cache/torch_extensions/py39_cu120 -out-root dpct_sycl -p ./compile_commands.json --use-experimental-features=all --rule-file=./stylegan3_specific_migration_rule.yaml
+
+
